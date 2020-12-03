c900133c51d3cdb0058d2ae3385281379813d8e6
Miloje Markovic <miloje.markovic@nosolutions.rs>
2020-12-03 14:11:32 +0100

    new commit

--- main.js
+++ main.js
@@ -1,38 +1,16 @@
 document.getElementById("navButton").addEventListener("click", toggleNav);
 
 function toggleNav(){
-    navSize = document.getElementById("mySidenav").style.width;
-    if (navSize === '250px') {
-        return close();
-    }
-    return open();
+  navSize = document.getElementById("mySidenav").style.width;
+  if (navSize === '250px') {
+    return close();
+  }
+  return open();
 }
 
 function open() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 function close() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 7000);
+  document.getElementById("mySidenav").style.width = "0";
 }
