babf7e08654609596b713ae0c3d4fa819f42b97b
Miloje Markovic <miloje.markovic@nosolutions.rs>
2020-12-07 16:05:28 +0100

    new commit for landing page

--- slider.js
+++ slider.js
@@ -24,7 +24,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000);
 }
 
 // function showSlide() {
